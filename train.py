69deb86c49f2548ef7ddc58a57859fe2a49e7531
ttoommmmiiee <tommie@blackshuck.co>
2023-03-20 11:07:49 +0000

    trained VAE with FFSD dataset

--- train.py
+++ train.py
@@ -1,27 +1,34 @@
-from tensorflow.keras.datasets import mnist
+import os
+import numpy as np
 from vae import VAE
 
+
 LEARNING_RATE = 0.0005
-BATCH_SIZE = 32
-EPOCHS = 20
+BATCH_SIZE = 64
+EPOCHS = 150
 
-def load_mnist():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+SPECTROGRAMS_PATH = "./dataset/free-spoken-digit-dataset-master/spectrograms/"
 
-    x_train = x_train.astype("float32") / 255
-    x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = x_test.astype("float32") / 255
-    x_test = x_test.reshape(x_test.shape + (1,))
+def load_ffsd(spectrograms_path,):
+    x_train = []
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path)
+            x_train.append(spectrogram)
+    x_train = np.array(x_train)
+    x_train = x_train[...,np.newaxis]
+    return x_train
 
-    return x_train, y_train, x_test, y_test
+
 
 def train(x_train, learning_rate, batch_size, epochs):
     vae = VAE(
-        input_shape=(28, 28, 1),
-        conv_filters=(32, 64, 64, 64),
-        conv_kernels=(3,  3,  3,  3),
-        conv_strides=(1,  2,  2,  1),
-        latent_space_dim=2 
+        input_shape=(256, 64, 1),
+        conv_filters=(512, 256, 128, 64, 32),
+        conv_kernels=(3,  3,  3,  3, 3),
+        conv_strides=(2,  2,  2,  2, (2, 1)),
+        latent_space_dim=128 
     )
     vae.summary()
     vae.compile(learning_rate)
@@ -31,6 +38,6 @@ def train(x_train, learning_rate, batch_size, epochs):
     
 
 if __name__ == "__main__":
-    x_train, y_train, x_test, y_test = load_mnist()
+    x_train = load_ffsd(SPECTROGRAMS_PATH)
     vae = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
     vae.save("model")
